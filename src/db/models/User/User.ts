3f2c77f3afae6f11249ec15fb753ffb6ebe1931b
parkdigy <parkdigy@gmail.com>
2026-01-09 02:29:29 +0900

    Update

--- src/db/models/User/User.ts
+++ src/db/models/User/User.ts
@@ -3,13 +3,8 @@
  * ******************************************************************************************************************/
 
 import { Knex } from 'knex';
-import { makeEnum } from '../@util';
 import { type TableInsertData, type TableUpdateData } from '../@types';
-
-/** 상태 */
-const Status = { ON: '사용', OFF: '미사용', LOCK: '잠금' };
-export type TUser$Status = keyof typeof Status;
-export const TUser$Status = makeEnum('status', Status);
+import { type UserStatus } from '@db_types';
 
 export interface TUser {
   /** Primary Key */
@@ -18,7 +13,7 @@ export interface TUser {
   email: string; // 이메일 // max:100
   password: string; // 비밀번호 // max:500
   login_fail_count: number; // 로그인 실패 횟수 // int, default:0
-  status: TUser$Status; // 상태 // default:ON
+  status: UserStatus; // 상태 // default:ON
   create_date: Date; // 등록일자
   update_date: Date; // 수정일자
 }
